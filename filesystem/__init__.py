5a873ce41c02d97e2573ecb92626737abb6b1a8c
Der Coop <dercoop@users.sourceforge.net>
2014-07-01 22:47:14 +0200

    add filesystem helper

---
+++ filesystem/__init__.py
@@ -0,0 +1,43 @@
+"""
+Some filesystem helper functions for python
+"""
+
+__author__ = 'coop'
+__license__ = 'GPLv2'
+
+import subprocess
+
+def mount(partition, target, fs=None, options=None):
+    """mount a partition
+
+    Arguments:
+    partition:  the partition to mount
+    target:     the target where the partition should be mounted
+    fs:         the filesystem of the partition to mount (optional)
+    options:    the options (rw, ro ...) (optional)
+
+    Return:
+    The returncode of the mount command
+    """
+    cmd = ['mount', '-o', 'ro']
+    if fs:
+        cmd.extend(['-t', fs])
+    if options:
+        cmd.extend(['-o', options])
+    cmd.append(partition)
+    cmd.append(target)
+    return subprocess.check_call(cmd)
+
+
+def umount(target):
+    """unmount a partition
+
+    Arguments:
+    target: the target to unmount
+
+    Return:
+    The returncode of the mount command
+    """
+    cmd = ['umount', target]
+    return subprocess.check_call(cmd)
+
